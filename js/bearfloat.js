0bffb465ff530aa30ed4ecbd11bb7aca8b3af2b4
froggie0503 <u28329330@gmail.com>
2025-09-06 22:24:13 +0800

    修復overflow 改吐司顏色 跟吐司首頁打叉問題

--- js/bearfloat.js
+++ js/bearfloat.js
@@ -3,13 +3,13 @@ const bear = document.getElementById('bearfloat-img');
 
 // 定义小球的移动范围 (以距离视窗底部的像素为单位)
 // 这些值现在是「距离底部」的距离
-const bottomMinDistance = 30;  // 距离视窗底部最近 50px
-const bottomMaxDistance = 55; // 距离视窗底部最远 100px
+const bottomMin = 30;  // 距离视窗底部最近 50px
+const bottomMax = 55; // 距离视窗底部最远 100px
 
 // 定义小球的初始位置和速度
 // 初始位置设定在范围内的某个点
 // 这里的 position 是「距离视窗顶部的距离」，需要从底部距离转换
-let position = bottomMinDistance;
+let position = bottomMin;
 let direction = 0.05; // 1 表示向下移动，-1 表示向上移动
 
 
@@ -20,12 +20,12 @@ function animateBear() {
     position +=  direction;
 
     
-    if (position > bottomMaxDistance) {
+    if (position > bottomMax) {
         // 向下移动
         direction = -0.05;
     }
     
-    else if (position < bottomMinDistance) {
+    else if (position < bottomMin) {
         // 向上移動
         direction = 0.05;
     }
